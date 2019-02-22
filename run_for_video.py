f0f3900a7c4e5cb4d807fccf5f271facf1d55d5a
nnmhuy <nnmhuy@apcs.vn>
2019-02-22 08:40:26 +0700

    .

--- run_for_video.py
+++ run_for_video.py
@@ -435,8 +435,6 @@ for i in range(1, 2):
 		# cv2.imwrite('./images/second.png', nextFrame)
 		# arguments_strOut = '/content/drive/My Drive/AI city challenge 2019/flow/%d/flow%d.flo' % (i, index)
 		arguments_strOut = './flow/flow%d.flo' % index
-		if (index % 300 == 0):
-			print(arguments_strOut)
 		main(arguments_strOut, currentFrame, nextFrame)
 		currentFrame = nextFrame
 		index += 1
