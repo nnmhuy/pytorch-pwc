962eb004776731496e9b1670b2c9f535f5f8ee09
nnmhuy <nnmhuy@apcs.vn>
2019-02-22 08:50:11 +0700

    revert

--- run_for_video.py
+++ run_for_video.py
@@ -433,8 +433,7 @@ for i in range(1, 2):
 		ret, nextFrame = cap.read()
 		# cv2.imwrite('./images/first.png', currentFrame)
 		# cv2.imwrite('./images/second.png', nextFrame)
-		# arguments_strOut = '/content/drive/My Drive/AI city challenge 2019/flow/%d/flow%d.flo' % (i, index)
-		arguments_strOut = './flow/flow%d.flo' % index
+		arguments_strOut = '/content/drive/My Drive/AI city challenge 2019/flow/%d/flow%d.flo' % (i, index)
 		main(arguments_strOut, currentFrame, nextFrame)
 		currentFrame = nextFrame
 		index += 1
