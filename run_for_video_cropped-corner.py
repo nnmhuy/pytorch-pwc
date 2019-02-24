dca21164f44c26dd4cafd9c863b4f13b0d14717f
Thuc Nguyen <zthucnguyen99@gmail.com>
2019-02-24 11:32:24 +0700

    test

--- run_for_video_cropped-corner.py
+++ run_for_video_cropped-corner.py
@@ -442,10 +442,10 @@ for i in range(33, 34):
 	cap = cv2.VideoCapture('content/drive/My Drive/aic19-track3-train-data/%d.mp4' % i)
 	index = 0
 	ret, currentFrame = cap.read()
-	print('1')
 	while(cap.isOpened()):
 		ret, nextFrame = cap.read()
 		index += 1
+		print('1')
 		if (index % 2 == 1):
 			continue
 		if (index > 600):
