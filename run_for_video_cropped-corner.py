9bd9ed229a2492a5b90b1b5a2d8d307987957a4e
Thuc Nguyen <zthucnguyen99@gmail.com>
2019-02-27 01:01:00 +0700

    config time and video for test

--- run_for_video_cropped-corner.py
+++ run_for_video_cropped-corner.py
@@ -425,7 +425,7 @@ def main(arguments_strOut):
 # zoom corner
 def zoomImgCorner(img):
 	height, width = img.shape[:2]
-	crop_img = img[0:0+height/2, 0+width/2:0+width]
+	crop_img = img[0:0+height/2, 0:0+width/2]
 
 	res = cv2.resize(crop_img,(width, height), interpolation = cv2.INTER_CUBIC)
 	return res
